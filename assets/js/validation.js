7826015b6d3eea524102edae2deb38df267a81b1
fniyonshuti <f.niyonshut1@alustudent.com>
2024-06-18 00:07:55 +0200

    added expiring date validation

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -8,3 +8,14 @@ document.getElementById('full-name').addEventListener('input', function () {
         document.getElementById('full-name-error').textContent = '';
     }
 });
+
+// Author: @fniyonshuti
+document.getElementById('expiring-date').addEventListener('input', function () {
+    const expiringDate = this.value;
+    const regex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+    if (!regex.test(expiringDate)) {
+        document.getElementById('expiring-date-error').textContent = 'Invalid date format. Use MM/YY.';
+    } else {
+        document.getElementById('expiring-date-error').textContent = '';
+    }
+});
